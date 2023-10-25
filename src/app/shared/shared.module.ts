23290f863ef629887249809be8e8bc372ec9d660
Joselu <joseluissgz97@gmail.com>
2023-10-25 22:12:58 +0200

    se importa search-box component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     AboutPageComponent,
     SidebarComponent,
     ContactPageComponent,
+    SearchBoxComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
@@ -20,6 +22,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     AboutPageComponent,
     SidebarComponent,
     ContactPageComponent,
+    SearchBoxComponent,
   ],
 })
 export class SharedModule {}
