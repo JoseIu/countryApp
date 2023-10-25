b230a5fc2d4a31c6f87f58f3c2010ca5320e963c
Joselu <joseluissgz97@gmail.com>
2023-10-25 22:09:22 +0200

    comoponente by-capitañ

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styleUrls: ['./by-capital-page.component.scss']
+  styleUrls: ['./by-capital-page.component.scss'],
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
 
+  constructor(private countriesService: CountriesService) {}
+  searchByCapital(value: string) {
+    this.countriesService.searchCapital(value).subscribe((countries) => {
+      this.countries = countries;
+      console.log(this.countries);
+    });
+  }
 }
