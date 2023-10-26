a219a725b9be03afa183fb3af4d71086f440e56d
Joselu <joseluissgz97@gmail.com>
2023-10-26 17:35:58 +0200

    by-country component

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styleUrls: ['./by-country-page.component.scss']
+  styleUrls: ['./by-country-page.component.scss'],
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
 
+  searchCountry(term: string) {
+    this.countriesService
+      .searchCountry(term)
+      .subscribe((countries) => (this.countries = countries));
+  }
 }
