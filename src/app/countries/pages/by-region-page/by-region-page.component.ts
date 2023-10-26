208598efcfaca37f6fa6f06c42558fd0d0f6a70c
Joselu <joseluissgz97@gmail.com>
2023-10-26 17:36:13 +0200

    by-region component

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styleUrls: ['./by-region-page.component.scss']
+  styleUrls: ['./by-region-page.component.scss'],
 })
 export class ByRegionPageComponent {
+  public countries: Country[] = [];
 
+  constructor(private countriesService: CountriesService) {}
+
+  searchRegion(term: string) {
+    this.countriesService
+      .seratchRegion(term)
+      .subscribe((countries) => (this.countries = countries));
+  }
 }
