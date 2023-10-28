5d9aed3c6aa7424806639d3b91d559f40129dde0
Joselu <joseluissgz97@gmail.com>
2023-10-28 16:30:28 +0200

    refactoring services

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,35 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiURL: string = 'https://restcountries.com/v3.1';
+
+  private searchCountryRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => of([])),
+      delay(2000)
+    );
+  }
+
   constructor(private http: HttpClient) {}
 
   searchCountryByID(id: string): Observable<Country | null> {
-    return this.http.get<Country[]>(`${this.apiURL}/alpha/${id}`).pipe(
+    const url = `${this.apiURL}/alpha/${id}`;
+
+    return this.http.get<Country[]>(url).pipe(
       map((countries) => (countries.length > 0 ? countries[0] : null)),
       catchError((error) => of(null))
     );
   }
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.http
-      .get<Country[]>(`${this.apiURL}/capital/${term}`)
-      .pipe(catchError((error) => of([])));
+    const url = `${this.apiURL}/capital/${term}`;
+    return this.searchCountryRequest(url);
   }
 
   searchCountry(term: string): Observable<Country[]> {
-    return this.http
-      .get<Country[]>(`${this.apiURL}/name/${term}`)
-      .pipe(catchError((error) => of([])));
+    const url = `${this.apiURL}/name/${term}`;
+
+    return this.searchCountryRequest(url);
   }
 
   seratchRegion(term: string): Observable<Country[]> {
-    return this.http
-      .get<Country[]>(`${this.apiURL}/region/${term}`)
-      .pipe(catchError((error) => of([])));
+    const url = `${this.apiURL}/region/${term}`;
+
+    return this.searchCountryRequest(url);
   }
 }
