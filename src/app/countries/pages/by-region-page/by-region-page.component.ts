9f8099b35443b9eeab5acfd9bd92975a9e846f3a
Joselu <joseluissgz97@gmail.com>
2023-10-29 11:59:34 +0100

    adding list of regions, to click

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,10 +11,13 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchRegion(term: string) {
+  searchRegion(term: Region) {
+    this.selectedRegion = term;
     this.countriesService
       .seratchRegion(term)
       .subscribe((countries) => (this.countries = countries));
