95e30c3a599bceac4187a8cf5a501dbb6bf0d5a4
Joselu <joseluissgz97@gmail.com>
2023-10-29 11:58:43 +0100

    limpiando subscribes

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss'],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   //Recivimos el placeHolder
   @Input()
@@ -24,6 +32,9 @@ export class SearchBoxComponent implements OnInit {
     this.debouncer
       .pipe(debounceTime(300))
       .subscribe((value) => this.onDebouncer.emit(value));
+  }
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe;
   }
 
   // emitValue(value: string): void {
